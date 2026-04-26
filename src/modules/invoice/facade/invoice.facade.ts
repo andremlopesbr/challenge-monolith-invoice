e93a04fcf4ff89f4f95ed87c7b2c8735e3bed113
André Lopes <artedesignpa@gmail.com>
2026-04-26 01:08:32 -0300

    Create invoice.facade.ts

---
+++ src/modules/invoice/facade/invoice.facade.ts
@@ -0,0 +1,30 @@
+import UseCaseInterface from "../../@shared/usecase/use-case.interface";
+import InvoiceFacadeInterface, {
+    FindInvoiceFacadeInputDTO,
+    FindInvoiceFacadeOutputDTO,
+    GenerateInvoiceFacadeInputDto,
+    GenerateInvoiceFacadeOutputDto,
+} from "./invoice.facade.interface";
+
+export interface UseCaseProps {
+    findUseCase: UseCaseInterface;
+    generateUseCase: UseCaseInterface;
+}
+
+export default class InvoiceFacade implements InvoiceFacadeInterface {
+    private _findUseCase: UseCaseInterface;
+    private _generateUseCase: UseCaseInterface;
+
+  constructor(usecaseProps: UseCaseProps) {
+        this._findUseCase = usecaseProps.findUseCase;
+        this._generateUseCase = usecaseProps.generateUseCase;
+  }
+
+  async find(input: FindInvoiceFacadeInputDTO): Promise<FindInvoiceFacadeOutputDTO> {
+        return await this._findUseCase.execute(input);
+  }
+
+  async generate(input: GenerateInvoiceFacadeInputDto): Promise<GenerateInvoiceFacadeOutputDto> {
+        return await this._generateUseCase.execute(input);
+  }
+}
