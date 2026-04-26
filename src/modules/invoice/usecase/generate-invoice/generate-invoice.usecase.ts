e56892eac408e6399c245827e8d57cdfa69d2981
André Lopes <artedesignpa@gmail.com>
2026-04-26 00:44:40 -0300

    Create generate-invoice.usecase.ts

---
+++ src/modules/invoice/usecase/generate-invoice/generate-invoice.usecase.ts
@@ -0,0 +1,58 @@
+import Id from "../../../@shared/domain/value-object/id.value-object";
+import Address from "../../domain/address.value-object";
+import Invoice from "../../domain/invoice.entity";
+import InvoiceItem from "../../domain/invoice-item.entity";
+import InvoiceGateway from "../../gateway/invoice.gateway";
+import {
+    GenerateInvoiceUseCaseInputDto,
+    GenerateInvoiceUseCaseOutputDto,
+} from "./generate-invoice.dto";
+
+export default class GenerateInvoiceUseCase {
+    constructor(private readonly invoiceGateway: InvoiceGateway) {}
+
+  async execute(
+        input: GenerateInvoiceUseCaseInputDto
+      ): Promise<GenerateInvoiceUseCaseOutputDto> {
+        const props = {
+                name: input.name,
+                document: input.document,
+                address: new Address({
+                          street: input.street,
+                          number: input.number,
+                          complement: input.complement,
+                          city: input.city,
+                          state: input.state,
+                          zipCode: input.zipCode,
+                }),
+                items: input.items.map((item) => {
+                          return new InvoiceItem({
+                                      id: new Id(item.id),
+                                      name: item.name,
+                                      price: item.price,
+                          });
+                }),
+        };
+
+      const invoice = new Invoice(props);
+        await this.invoiceGateway.generate(invoice);
+
+      return {
+              id: invoice.id.id,
+              name: invoice.name,
+              document: invoice.document,
+              street: invoice.address.street,
+              number: invoice.address.number,
+              complement: invoice.address.complement,
+              city: invoice.address.city,
+              state: invoice.address.state,
+              zipCode: invoice.address.zipCode,
+              items: invoice.items.map((item) => ({
+                        id: item.id.id,
+                        name: item.name,
+                        price: item.price,
+              })),
+              total: invoice.total,
+      };
+  }
+}
